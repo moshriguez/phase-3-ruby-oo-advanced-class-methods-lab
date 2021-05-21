effaf5675ccaf9531fc4dc9b59c28f70f845f587
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-05-20 22:42:28 -0700

    commit tree 05883d0f565dee558f90463cc93f38944a663c27

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,56 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    new_song = self.new
+    new_song.save
+    new_song
+  end
+
+  def self.new_by_name(song)
+    new_song = self.new
+    new_song.name = song
+    new_song
+  end
+
+  def self.create_by_name(song)
+    new_song = self.new
+    new_song.name = song
+    new_song.save
+    new_song
+  end
+
+  def self.find_by_name(song_query)
+    self.all.find {|song| song.name == song_query}
+  end 
+
+  def self.find_or_create_by_name(song_query)
+    if self.find_by_name(song_query)
+      self.find_by_name(song_query)
+    else
+      self.create_by_name(song_query)
+    end
+  end
+
+  def self.alphabetical
+    self.all.sort {|a, b| a.name <=> b.name}
+  end
+
+  def self.new_from_filename(filename) #filename format "Taylor Swift - Blank Space.mp3"
+    song_artist_array = filename.sub(".mp3", '').split(" - ")
+    new_song = self.create_by_name(song_artist_array[1])
+    new_song.artist_name = song_artist_array[0]
+    new_song
+  end
+  
+  def self.create_from_filename(filename)
+    song_artist_array = filename.sub(".mp3", '').split(" - ")
+    new_song = self.create_by_name(song_artist_array[1])
+    new_song.artist_name = song_artist_array[0]
+  end
+
+  def self.destroy_all
+    @@all = []
+  end
+
 end
